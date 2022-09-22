f8ba67ebb673c08b32dcfb7342bdab9c946e7cf1
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-09-21 21:53:22 -0700

    edit hello-world

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 
  let usersName = prompt ("Are you a Teacher, TA or Student?");
  
- console.log("Ciao!" + usersName + "welcome!");
+ console.log("Ciao!"  +  "welcome!");
 
  document.write("Ciao!" + usersName + "welcome!");
 
@@ -10,7 +10,8 @@
  } else if (usersName == "Teacher") {
     alert("Eh, figurati….  Well, what a surprise….");
  } else if (usersName == "TA") {
-    alert("Posso dire quel che mi va. I can say whatever I want.")
+    alert("Posso dire quel che mi va. I can say whatever I want.");
  } else {
     alert("Divertiti! Have a good time!");
  }
+ 
