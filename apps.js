00f7f0af0cc21fe7315dd45cf2ded6a00caa9f71
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-09-22 13:18:44 -0700

    update hello-world

--- apps.js
+++ apps.js
@@ -1,25 +1,36 @@
+let usersName;
 
- let usersName;
- 
- function greetUser() {
-   usersName = prompt ("Are you a Teacher, TA or Student?");
-   if (usersName == ""){
-      usersName = prompt("Who are you?")
+function greetUser(){
+   let usersName = prompt("Are you a Teacher, TA or Student?");
+   if (usersName == "") {
+      usersName = prompt("Who are you?");
    }
+   console.log(usersName);
+   document.write("Ciao " + usersName + "! Welcome!");
+   return usersName;
+}
+greetUser();
+let foodQuestion = prompt ("Do you like to eat??")
+console.log(foodQuestion)
+
+if (foodQuestion == "Yes") or ("yes") 
+alert("Mangiamo! Lets eat!");
+if (foodQuestion == "No") or ("no")
+   alert("Poverino! You poor thing!");
+ //let usersName = prompt ("Are you a Teacher, TA or Student?");
  
- console.log("Ciao"  + usersName + ! "Welcome!");
- document.write("Ciao!" + usersName + ! "welcome!");
- return usersName;
+ //console.log("Ciao "  +  "! Welcome!");
 
- greetUser();
-
- if (usersName == "Student || student") {
+ //document.write("Ciao " + usersName + "! Welcome!");
+function customAlert() {
+ if (usersName == "Student" || "student") {
     alert("Dai! Veramente? Come on! Really?");
- } else if (usersName == "Teacher || teacher") {
-    alert("Eh, figurati….  Well, what a surprise….");
- } else if (usersName == "TA || ta") {
+ } else if (usersName == "Teacher" || "teacher") {
+     alert("Eh, figurati….  Well, what a surprise….");
+ } else if (usersName == "TA" || "ta") {
     alert("Posso dire quel che mi va. I can say whatever I want.");
  } else {
     alert("Divertiti! Have a good time!");
  }
- 
+
+ customAlert();} 
